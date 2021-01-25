1ce0282f26e1dd0d4c9066f5d7844e5a618a6904
Henrique Dias <henrique.dias@gmail.com>
2021-01-25 16:40:59 -0300

    Final touch :)

---
+++ src/csp/resources/js/customdash.js
@@ -0,0 +1,93 @@
+var urlOrigin = window.location.origin;
+var urlREST = urlOrigin + "/csp/irisapp/api";
+
+$(document).ready(function () {
+  
+
+  $.getJSON(urlREST + "/global/header", function(response) {
+    $("#idTotalVaccines").text(response.totalVaccine);
+    $("#idTotalCountry").text(response.totalCountries);
+    $("#idTotal").text(response.totalVaccination.toLocaleString());
+  });
+
+  $.getJSON(urlREST + "/global/topvacbycountry", function(response) {
+
+    var options = {
+      chart: {
+        height: 380,
+        width: "100%",
+        type: "line"
+      },
+      stroke: {
+        show: true,
+        curve: 'smooth'
+      },
+      series: response,
+      xaxis: {
+        type: "datetime"
+      }
+    };
+  
+    var chart = new ApexCharts(document.querySelector("#chartline"), options);
+  
+    chart.render();
+  });
+
+
+  $.getJSON(urlREST + "/global/topvacbarchart", function(response) {
+
+    var chart4 = new ApexCharts(document.querySelector("#apexchart-4"), {
+      series: [{
+        name: "Total Vaccinations",
+        data: response[0]
+      }],
+      chart: {
+          type: "bar"
+      },
+      plotOptions: {
+          bar: {
+              horizontal: true
+          }
+      },
+      dataLabels: {
+          enabled: false
+      },
+      xaxis: {
+        categories: response[1]
+      }
+    });
+  
+    chart4.render();
+
+  });
+
+
+  $.getJSON(urlREST + "/global/vaccineperc", function(response) {
+    var chart8 = new ApexCharts(document.querySelector("#piechart"),  {
+      series: response[0],
+      chart: {
+          type: "pie"
+      },
+      labels: response[1],
+      tooltip: {
+          fillSeriesColor: false
+      },
+      responsive: [{
+          breakpoint: 480,
+          options: {
+              chart: {
+                  width: 200
+              },
+              legend: {
+                  position: "bottom"
+              }
+          }
+      }]
+    });
+
+    chart8.render();
+  });
+  
+  
+
+});
